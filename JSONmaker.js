89b788600e6c17df73f12d4ad5bd0cd111338956
MSL19 <40368820+MSL19@users.noreply.github.com>
2019-04-15 15:58:46 -0600

    Working on having it send out a JSON object with all the data
    The problem of what to do about the .JSON files looms large

--- JSONmaker.js
+++ JSONmaker.js
@@ -1,4 +1,47 @@
+//time stuff
+let date;
+let time;
+let lastRef;
+let year;
+let mon;
+let day;
+let minutes;
+let minutesT;
+let lastDBTime;
+function updateTime(){
+    d = new Date();
+    year = d.getFullYear();
+    mon = d.getMonth()+1;
+    day = d.getDate();
+    hours = d.getHours(); //for the kdsatp server i don't need to subtract 2
+    minutes = d.getMinutes();
+/*(minutes<0){
+    hours--;
+}*/
+if(minutes<30){
+     minutesT = "00";
+}
+else{
+     minutesT = "30";
+}
+time = hours+":"+minutesT+":00";
+
+var dw = d.getDay();
+if(dw==6){ //stocks don't trade over the weekend
+day--;
+}
+if(dw==0){
+day-=2;
+}
+if(day<10){
+day = "0"+day;
+}
+date = year+"-"+mon+"-"+day;
+}
+
+
 var tf = require('@tensorflow/tfjs');
+const googleTrends = require('google-trends-api');
 var https = require("https");
 const fs = require('fs'); 
 let model;
@@ -9,10 +52,60 @@ let pricesDeltaH = [];
 let volumeDeltaH = [];
 let priceMovementH = [];
 let shiftedPriceChange =[];
+
+let pricesDeltaHM = [];
+let volumeDeltaHM = [];
+let priceMovementHM = [];
+let shiftedPriceChangeM =[];
+
 let JSONdata = require('./stockTraining.json');
 let testData = require('./predictJSON.json');
-function getStockJSON(){ //this pulls the JSON data on Apple stock from Alphavantage and returns the JSON
+
+let bigString = {};
+
+async function updateNN(){
+    predictPrice(); 
+
+}  
+
+async function predictPrice(){ //this function only runs of the stock price has updated (this happens every 30 minutes)
+try{
+    let comp = await getStockJSON();
+    let DBup = dataBaseCheck(comp); //checking if the Database has updated--if not, the NN will not be run
+    if(DBup){
+        
+        predictData = tf.tensor2d(testData.map(item=>[
+            item.pD, item.vD, item.pDM, item.vDM
+        ]
+        ), [1,4]
+        ) 
+
+    }
+}
+
+}
+
+function dataBaseCheck(company){ //this checks to see if the database has been updated
+    let SMupdateBool; 
+   
+    
+    lastRef = company['Meta Data']['3. Last Refreshed'];
+    console.log(lastRef);
+    if(lastRef !== lastDBTime){
+        SMupdateBool = true;
+        lastDBTime = lastRef;
+    }
+    else{
+        SMupdateBool = false;
+    }
+    
+   return SMupdateBool;
+   
+    }
+
+function getStockJSONapple(){ //this pulls the JSON data on Apple stock from Alphavantage and returns the JSON
     return new Promise(function(resolve, reject){
+        
         var request = https.request({
             method: "GET",
             host: "www.alphavantage.co", //"api.intrinio.com",
@@ -28,6 +121,36 @@ function getStockJSON(){ //this pulls the JSON data on Apple stock from Alphavan
                 try{
                 company = JSON.parse(json);   
                 resolve(company); //returning the JSON
+                console.log(company);
+                }
+                catch(e){
+                reject(e);
+                }                
+            });
+        });
+        request.end();
+        });
+         
+  }
+  function getStockJSONmicro(){ //this pulls the JSON data on Apple stock from Alphavantage and returns the JSON
+    return new Promise(function(resolve, reject){
+        var request = https.request({
+            method: "GET",
+            host: "www.alphavantage.co", //"api.intrinio.com",
+            path: "/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=30min&apikey=HQ5I4BGWLZBZUPJC", 
+            
+        
+        }, function(response) {
+            var json = "";
+            response.on('data', function (chunk) {
+                json += chunk;
+            });
+            response.on('end', function() {
+                try{
+                company = JSON.parse(json);   
+                resolve(company); //returning the JSON
+                console.log(company);
+
                 }
                 catch(e){
                 reject(e);
@@ -39,8 +162,11 @@ function getStockJSON(){ //this pulls the JSON data on Apple stock from Alphavan
          
   }
   let jsonString;
+  let jsonString2;
   async function getData(){
-  jsonString = await getStockJSON();
+  jsonString = await getStockJSONapple();
+  jsonString2 = await getStockJSONmicro();
+  
   parseData();
   }
   getData();
@@ -54,24 +180,36 @@ function getStockJSON(){ //this pulls the JSON data on Apple stock from Alphavan
     let volumtDelta = jsonString["Time Series (30min)"][timeSeriesKeys[i-1]]['5. volume'] - jsonString["Time Series (30min)"][timeSeriesKeys[i]]['5. volume'];
     volumeDeltaH.push(volumtDelta);
   
+    let priceDeltaM = jsonString2["Time Series (30min)"][timeSeriesKeys[i-1]]['4. close'] - jsonString["Time Series (30min)"][timeSeriesKeys[i]]['4. close'];
+    pricesDeltaHM.push(priceDeltaM);
+    let volumtDeltaM = jsonString2["Time Series (30min)"][timeSeriesKeys[i-1]]['5. volume'] - jsonString["Time Series (30min)"][timeSeriesKeys[i]]['5. volume'];
+    volumeDeltaHM.push(volumtDeltaM);
     
   }
   for(let i = timeSeriesKeys.length-2; i>=1; i--){
     let priceDelta = jsonString["Time Series (30min)"][timeSeriesKeys[i-1]]['4. close'] - jsonString["Time Series (30min)"][timeSeriesKeys[i]]['4. close'];
     shiftedPriceChange.push(priceDelta);
+
+  /*  let priceDeltaM = jsonString2["Time Series (30min)"][timeSeriesKeys[i-1]]['4. close'] - jsonString["Time Series (30min)"][timeSeriesKeys[i]]['4. close'];
+    shiftedPriceChangeM.push(priceDeltaM);*/
  }
   let jData = [];
   for(let i= 0; i<volumeDeltaH.length; i++){
       let objectP = {
-          pD: pricesDeltaH[i],
-          vD: volumeDeltaH[i], 
-            pC: shiftedPriceChange[i]
+        pD: pricesDeltaH[i],
+        vD: volumeDeltaH[i], 
+        pDM: pricesDeltaHM[i],
+        vDM: volumeDeltaHM[i],
+        pC: shiftedPriceChange[i]
+
               }
       jData.push(objectP);
   }
   testData = [{
       pD: jsonString["Time Series (30min)"][timeSeriesKeys[0]]['4. close'] - jsonString["Time Series (30min)"][timeSeriesKeys[1]]['4. close'], 
-      vD: jsonString["Time Series (30min)"][timeSeriesKeys[0]]['5. volume'] - jsonString["Time Series (30min)"][timeSeriesKeys[1]]['5. volume']
+      vD: jsonString["Time Series (30min)"][timeSeriesKeys[0]]['5. volume'] - jsonString["Time Series (30min)"][timeSeriesKeys[1]]['5. volume'],
+      pDM: jsonString2["Time Series (30min)"][timeSeriesKeys[0]]['4. close'] - jsonString2["Time Series (30min)"][timeSeriesKeys[1]]['4. close'],
+      vDM: jsonString2["Time Series (30min)"][timeSeriesKeys[0]]['5. volume'] - jsonString2["Time Series (30min)"][timeSeriesKeys[1]]['5. volume']
   }]
   fs.writeFile('stockTraining.json', JSON.stringify(jData), function(){});
   fs.writeFile('predictJSON.json', JSON.stringify(testData), function(){});
@@ -85,9 +223,9 @@ function trainNN(){
 
  model = tf.sequential();
     trainingData = tf.tensor2d(JSONdata.map(item=> [
-        item.pD, item.vD
+        item.pD, item.vD, item.pDM, item.vDM
     ]
-    ),[98,2])
+    ),[98,4])
 
      outputData = tf.tensor2d(JSONdata.map(item => [
         item.pC > 0  ? 1 : 0,
@@ -96,12 +234,12 @@ function trainNN(){
     
     ]), [98,3])
     predictData = tf.tensor2d(testData.map(item=>[
-        item.pD, item.vD
+        item.pD, item.vD, item.pDM, item.vDM
     ]
-    ), [1,2]
+    ), [1,4]
     )
     model.add(tf.layers.dense(
-        {   inputShape: 2, 
+        {   inputShape: 4, 
             activation: 'sigmoid', 
             units: 10
         }
@@ -133,6 +271,7 @@ async function train_data(){
   }
   console.log('....Model Prediction .....')
   model.predict(predictData).print();
+
 }
 
  
